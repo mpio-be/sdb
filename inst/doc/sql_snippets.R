1e5a9eee4df223b97b842f283058c07db74bdf16
valcu <valcu@orn.mpg.de>
2017-03-06 08:42:14 +0100

    via push all

--- inst/doc/sql_snippets.R
+++ inst/doc/sql_snippets.R
@@ -1,20 +1,20 @@
 ## ------------------------------------------------------------------------
 require(sdb)
-Host = '127.0.0.1'
+Host = 'scidb.mpio.orn.mpg.de'
 
 id = snipSave(query = 'SELECT * FROM BTatWESTERHOLZ.BREEDING limit 1',
-            description = 'select the first line of the table', user = 'valcu', host = Host)
+            description = 'select the first line of the table', user = 'mihai', host = Host)
 
 ## ------------------------------------------------------------------------
-s = snipSearch("BTatWESTERHOLZ.BREEDING", user = 'valcu', host = Host)
+s = snipSearch("BTatWESTERHOLZ.BREEDING", user = 'mihai', host = Host)
 
 ## ------------------------------------------------------------------------
-s1 = snipFetch(1, user = 'valcu', host = Host)
+s1 = snipFetch(1, user = 'mihai', host = Host)
 
 
 ## ------------------------------------------------------------------------
 id
 
 ## ------------------------------------------------------------------------
-snipDrop(id, user = 'valcu', host = Host)
+snipDrop(id, user = 'mihai', host = Host)
 
