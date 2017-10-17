9d3aa3740855c77725ccd307f4d4f24d8767c72f
valcu <valcu@orn.mpg.de>
2017-10-17 14:31:18 +0200

    via push all

--- R/dbio_dbcon.R
+++ R/dbio_dbcon.R
@@ -9,20 +9,20 @@
 #' @param password  password ("" or NA means no password e.g. for localhost)
 #' @param database  database to connect to
 #' @param path      to credentials file (if different from the default)
-#' @param driver    MySQL or spatial_MySQL, ... Defaults to MySQL.
+#' @param driver    MariaDB or spatial_MySQL, ... Defaults to MariaDB
 #' @param ...       pass to dbConnect
 #' @export
 #' @return          a connection object
 #' @seealso         \code{\link{saveCredentials}}, \code{\link{dbq} }
 
-dbcon <- function(user, pwd = "", db = NA, host = "scidb.mpio.orn.mpg.de", path, driver = "MySQL" , ...) {
+dbcon <- function(user, pwd = "", db = NA, host = "scidb.mpio.orn.mpg.de", path, driver = "MariaDB" , ...) {
 
   if(!missing(user) & !missing(pwd) )
   X = data.frame(user, pwd, db, host, stringsAsFactors = FALSE) else
   X = getCredentials(user = user, host = host, path = path)
   if( nrow(X) == 0 || is.na(X$user) ) stop( "Credentials for user ", dQuote(user), " are not saved!")
 
-  if( driver ==  "MySQL" ) {
+  if( driver ==  "MariaDB" ) {
     if(is.na(X$pwd) | X$pwd == "")
          con = dbConnect( dbDriver(driver) , user = X$user,                   host = X$host, ...) else
          con = dbConnect( dbDriver(driver) , user = X$user, password = X$pwd, host = X$host, ...)
