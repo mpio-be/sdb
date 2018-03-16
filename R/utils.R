c0fbaa0a31918f1517cf23c6f642d9639e204e15
valcu <valcu@orn.mpg.de>
2018-03-16 13:09:17 +0100

    probeDB()

--- R/utils.R
+++ R/utils.R
@@ -31,11 +31,39 @@ removeDuplicates <- function(con, table, key = 'pk') {
 #' string_is_mysql_date
 #' @export
 string_is_mysql_date <- function(x) {
-    o = str_detect(x, pattern = '(\\d{2}|\\d{4})(?:\\-)?([0]{1}\\d{1}|[1]{1}[0-2]{1})(?:\\-)?([0-2]{1}\\d{1}|[3]{1}[0-1]{1})(?:\\s)?([0-1]{1}\\d{1}|[2]{1}[0-3]{1})(?::)?([0-5]{1}\\d{1})(?::)?([0-5]{1}\\d{1})')
-    if ( all(is.na(o))  ) o = FALSE else o =  all(o, na.rm= TRUE)
-    o
-    }
+  o = str_detect(x, pattern = '(\\d{2}|\\d{4})(?:\\-)?([0]{1}\\d{1}|[1]{1}[0-2]{1})(?:\\-)?([0-2]{1}\\d{1}|[3]{1}[0-1]{1})(?:\\s)?([0-1]{1}\\d{1}|[2]{1}[0-3]{1})(?::)?([0-5]{1}\\d{1})(?::)?([0-5]{1}\\d{1})')
+  if ( all(is.na(o))  ) o = FALSE else o =  all(o, na.rm= TRUE)
+  o
+  }
 
 
-
+#' Probe a db server 
+#' @description Fast probe a db server see if it can be reached through a given port. Default to scidb.mpio.orn.mpg.de and port 3306
+#' @param   name default to scidb.mpio.orn.mpg.de
+#' @param   port default to 3306 (mariadb's default port )
+#' @return when reachable return the name of probed server else falls back to localhost (127.0.0.1)
+#' @export
+#' @examples \dontrun{
+#' probeDB()
+#' 
+#'}
    
+probeDB <- function(name = 'scidb.mpio.orn.mpg.de', port = 3306) {
+  
+    noBash = try(system('bash', intern = TRUE, ignore.stderr = TRUE), silent = TRUE) %>% inherits(.,'try-error') 
+
+    if(noBash) {
+      warning(paste('bash is not available so I cannot probe', name, '; host is  set to localhost.') )
+      host = '127.0.0.1'
+    } else {
+
+      syscall = paste0("timeout 0.5  bash -c 'cat < /dev/null > /dev/tcp/", name , "/", port, "'")
+      ans = suppressWarnings( system(syscall, intern = TRUE,ignore.stderr = TRUE) %>% attributes)
+      host = if( is.null(ans) ) name else '127.0.0.1'
+
+      }
+
+   if (name %in% c('localhost', '127.0.0.1') ) host = '127.0.0.1'   
+
+  host
+ }
